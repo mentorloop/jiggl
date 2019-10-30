a97d2b5ee5773aebd5531973bd7801474ef1ee3d
Mark Reid <contact@markreid.com.au>
2019-10-31 09:48:37 +1100

    Lots more things

--- README.md
+++ README.md
@@ -1,4 +1,56 @@
 # jiggl
 ### jira + toggl = jiggl
 
+Connect Jira to Toggl and print out reports.
 
+
+## Todo
+- detailed reports aren't linking all issues?
+- summary report: % of tickets with a jira issue
+  - or % of time? or both
+- sqlite cache for jira issues
+
+
+
+## Report types
+
+There's two basic types of reports, Summary and Detailed.
+
+
+### Summary report
+
+
+- Total time recorded
+- What was the project breakdown
+- What was the Jira issue breakdown (by parents)
+- What was the epic breakdown
+- % of tickets with a Jira issue
+- Can be interactively edited/aggregated (see *editing reports*)
+
+
+### Detailed report
+
+- Group by users
+- Aggregate time entries with the same name/project
+- Link to Jira issues
+- Highlight entries that can't be linked
+- Highlight entries with no description or project
+
+
+### Yesterday
+A detailed report for the last business day.
+
+
+### Last Month
+A summary report for the last calendar month.
+
+
+
+## Editing reports
+
+Summary reports can be interactively edited to merge/remove/rename entries.
+
+- For a list of 'items' with a .title and .time
+  - Remove one
+  - Merge with another one
+  - Rename it
--- index.js
+++ index.js
@@ -1,87 +1,186 @@
-const { getIssue } = require('./lib/jira');
-const { getSummary, groupAndSumItems } = require('./lib/toggl');
+const inquirer = require('inquirer');
+const inquirerDatepicker = require('inquirer-datepicker-prompt');
 
-async function getAugustSummary() {
-  return getSummary({
-    since: '2019-08-01',
-    until: '2019-08-31',
+const {
+  getIssuesForItems,
+  replaceIssuesWithParents,
+  replaceIssuesWithEpics,
+} = require('./lib/jira');
+const { getSummary, getDetailed } = require('./lib/toggl');
+const { editReport } = require('./lib/reports');
+const {
+  lastBusinessDay,
+  lastMonth,
+  twoDP,
+  useIssueTitles,
+  mergeItems,
+  compose,
+  formatTogglDate,
+} = require('./lib/util');
+const Doc = require('./lib/doc');
+
+inquirer.registerPrompt('datetime', inquirerDatepicker);
+
+async function getLastMonthSummary() {
+  const dates = lastMonth();
+  return getSummary(dates);
+}
+
+async function getYesterdayDetailed() {
+  const yesterday = lastBusinessDay();
+  return getDetailed(yesterday);
+}
+
+const cleanItems = items =>
+  items.map(({ title, time, percent }) => ({
+    title,
+    time,
+    percent,
+  }));
+
+const underline = (str, char = '=') => str.replace(/./g, char);
+
+const printReport = (title, report, numItems = 10) => {
+  console.log(title);
+  console.log(underline(title));
+  console.log(`Showing first ${numItems} of ${report.length}`);
+  console.log(cleanItems(report).slice(0, numItems || undefined));
+  console.log('\n\n\n');
+};
+
+async function processSummary(report) {
+  const issues = (await getIssuesForItems(report.items)).filter(
+    item => item.issueKey,
+  );
+  const issuesWithParents = compose(mergeItems, useIssueTitles)(await replaceIssuesWithParents(issues));
+  const epicIssues = mergeItems(await replaceIssuesWithEpics(issues));
+
+  const projects = await editReport(report.projects);
+  const epics = await editReport(epicIssues);
+  const features = await editReport(issuesWithParents);
+
+  process.stdout.write('\033c');
+
+  printReport('Toggl Projects', projects, 0);
+  printReport('Epic breakdown', epics, 0);
+  printReport('Features', features, 10);
+}
+
+async function processDetailed(report) {
+  // fetch issues
+  for (let userSummary of report.items) {
+    const itemsWithIssues = await getIssuesForItems(userSummary.items);
+    userSummary.items = itemsWithIssues;
+  }
+
+  const doc = new Doc();
+
+  const title = `Toggl Summary ${lastBusinessDay().since}`;
+  doc.log(title);
+  doc.log(underline(title));
+  doc.linebreak();
+  const usersWithProblems = new Set();
+  report.items.forEach(userSummary => {
+    const title = `${userSummary.user}: ${userSummary.time}`;
+    doc.log(title);
+    doc.log(underline(title, '-'));
+    useIssueTitles(userSummary.items).forEach(item => {
+      const title = item.issue
+        ? `[${item.issue.key}] ${item.issue.summary}`
+        : `* ${item.title || 'NO DESCRIPTION'}`;
+      const { project, time } = item;
+      const projectName = project
+        ? project.replace(/^Dev - /, '')
+        : 'NO PROJECT';
+      doc.log(`${title} - ${twoDP(time)} (${projectName})`);
+      if (!project || !item.title) {
+        usersWithProblems.add(userSummary.user);
+      }
+    });
+    doc.linebreak();
   });
+  if (usersWithProblems.size) {
+    doc.linebreak();
+    doc.log('Entries needing fixing');
+    doc.log(underline('Entries needing fixing'));
+    doc.log(
+      `${Array.from(usersWithProblems).join(' & ')} ${
+        usersWithProblems.size === 1 ? 'has' : 'have'
+      } entries with no description or project`,
+    );
+  }
+
+  doc.print();
 }
 
-async function getParentIssues(issues) {
-  // if an issue has a parent, grab that too.
-  const parentIssues = await Promise.all(
-    issues.map(issue => {
-      if (!issue.issue.parent) {
-        return Promise.resolve(issue);
-      }
-      console.log(
-        `Replacing ${issue.issue.key} with parent ${issue.issue.parent}`,
-      );
-      return getIssue(issue.issue.parent).then(parent => ({
-        title: parent.key,
-        time: issue.time,
-        issueKey: parent.key,
-        issue: parent,
-      }));
-    }),
-  );
-
-  // now re-group and sum again
-  return groupAndSumItems(parentIssues);
+async function runLastMonth() {
+  const report = await getLastMonthSummary();
+  return processSummary(report);
 }
 
-// group items by epics
-async function getEpics(issues) {
-  const epics = await Promise.all(
-    issues.map(issue => {
-      if (!issue.issue.epic) {
-        return Promise.resolve(issue);
-      }
-      console.log(
-        `Pulling epic ${issue.issue.epic} for issue ${issue.issue.key}`,
-      );
-      return getIssue(issue.issue.epic).then(epic => ({
-        title: epic.key,
-        time: issue.time,
-        issueKey: epic.key,
-        issue: epic,
-      }));
-    }),
-  );
-
-  return groupAndSumItems(epics.filter(issue => issue.issue.type === 'Epic'));
+async function runYesterday() {
+  const report = await getYesterdayDetailed();
+  return processDetailed(report);
 }
 
-const cleanIssue = issue => ({
-  title: `${issue.issueKey}: ${issue.issue.epicTitle || issue.issue.summary}`,
-  time: issue.time,
-});
-
-async function boot() {
-  const report = await getAugustSummary();
-
-  // pull the time entries that we could find issueKeys for,
-  // and fetch them from the server
-  const itemsWithKeys = report.items.filter(item => item.issueKey);
-
-  // todo - do this sequentially if we start hitting rate limits
-  const issues = await Promise.all(
-    itemsWithKeys.map(item =>
-      getIssue(item.issueKey).then(issue => ({
-        ...item,
-        issue,
-      })),
-    ),
-  );
-
-  const parents = await getParentIssues(issues);
-  const epics = await getEpics(issues);
-
-  console.log(parents.slice(0, 10).map(cleanIssue));
-  console.log(epics.map(cleanIssue));
-  console.log(report.total);
-  console.log(report.projects);
+async function runSummary() {
+  const dates = await promptForDates();
+  const report = await getSummary(dates);
+  return processSummary(report);
 }
 
-boot();
+async function runDetailed() {
+  const dates = await promptForDates();
+  const report = await getDetailed(dates);
+  return processDetailed(report);
+}
+
+const dateQuestion = {
+  type: 'datetime',
+  name: 'date',
+  message: 'Date',
+  format: ['yyyy', '-', 'mm', '-', 'dd'],
+};
+
+const promptForDates = () =>
+  inquirer
+    .prompt([
+      {
+        ...dateQuestion,
+        name: 'since',
+        message: 'Since',
+      },
+      {
+        ...dateQuestion,
+        name: 'until',
+        message: 'Until',
+      },
+    ])
+    .then(({ since, until }) => ({
+      since: formatTogglDate(since),
+      until: formatTogglDate(until),
+    }));
+
+inquirer
+  .prompt([
+    {
+      type: 'list',
+      choices: ['Yesterday', 'Last Month', 'Summary', 'Detailed'],
+      name: 'report',
+      message: 'what report do you want to run?',
+    },
+  ])
+  .then(({ report }) => {
+    switch (report) {
+      case 'Last Month':
+        return runLastMonth();
+      case 'Yesterday':
+        return runYesterday();
+      case 'Summary':
+        return runSummary();
+      case 'Detailed':
+        return runDetailed();
+      default:
+        return;
+    }
+  });
